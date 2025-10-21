e8c22bf90bf8d41f8d0f01965db65c9653c088d6
Rickz <91611484+Rick-cpp@users.noreply.github.com>
2025-10-20 21:27:40 -0300

    0.1.1
    Config pyproject

--- src/index.py
+++ src/index.py
@@ -1,9 +1,13 @@
-from packages.App.App import App
-import sys
+def main() -> None:
+    from packages.App.App import App
+    import sys
 
-from routes.Help import registerHelp
-from routes.Translator import registerTranslator
+    from routes.Help import registerHelp
+    from routes.Translator import registerTranslator
+    
+    registerHelp()
+    registerTranslator()
+    App.run(sys.argv[1:])
 
-registerHelp()
-registerTranslator()
-App.run(sys.argv[1:])
+if __name__ == "__main__":
+    main()
